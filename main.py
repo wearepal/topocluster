e89dd1855944ac4811ca9f730657c0067bdca648
Myles Bartlett <m.bartlett@sussex.ac.uk>
2020-11-02 21:08:53 +0000

    More updates.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from hydra.utils import instantiate
 from omegaconf import OmegaConf
 
 from gen.gudhi.clustering.tomato.conf import TomatoConf
+from gen.pytorch_lightning.conf import TrainerConf
 from gen.topocluster.clustering.kmeans.conf import KmeansConf
 from gen.topocluster.data.data_modules.conf import MNISTDataModuleConf
 from gen.topocluster.experiment.conf import ExperimentConf
@@ -12,21 +13,19 @@ from gen.topocluster.models.autoencoder.conf import GatedConvAutoEncoderConf
 
 # ConfigStore enables type validation
 cs = ConfigStore.instance()
-cs.store(name="primary", node=ExperimentConf)
+cs.store(name="experiment", node=ExperimentConf)
 cs.store(group="schema/datamodule", name="mnist", node=MNISTDataModuleConf, package="datamodule")
 cs.store(group="schema/encoder", name="gconv_ae", node=GatedConvAutoEncoderConf, package="encoder")
 cs.store(group="schema/clusterer", name="tomato", node=TomatoConf, package="clusterer")
 cs.store(group="schema/clusterer", name="kmeans", node=KmeansConf, package="clusterer")
+# cs.store(group="schema/trainer", name="trainer", node=TrainerConf, package="trainer")
+# cs.store(group="schema/trainer", name="pt_trainer", node=TrainerConf, package="trainer")
 
 
-@hydra.main(config_path="conf", config_name="primary")
+@hydra.main(config_path="conf", config_name="experiment")
 def launcher(cfg: ExperimentConf) -> None:
-
-    exp = instantiate(cfg)
-    exp.datamodule = instantiate(exp.datamodule)
-    exp.encoder = instantiate(exp.encoder)
-    exp.clusterer = instantiate(exp.clusterer)
-    exp.start(raw_config=OmegaConf.to_container(cfg))
+    exp = instantiate(cfg, _recursive_=True)
+    exp.start(OmegaConf.to_container(cfg))
 
 
 if __name__ == "__main__":
--- topocluster/experiment.py
+++ topocluster/experiment.py
@@ -1,8 +1,10 @@
 # """Main training file"""
+from collections import OrderedDict
 from dataclasses import dataclass
 from typing import Any, Dict, Optional, Sequence
 
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
 from torch.optim import Adam, Optimizer
 from torch.tensor import Tensor
 
@@ -19,35 +21,40 @@ class Experiment(pl.LightningModule):
         datamodule: pl.LightningDataModule,
         encoder: AutoEncoder,
         clusterer: Clusterer,
-        epochs: int,
-        pretrain_epochs: int,
+        trainer: pl.Trainer,
+        pt_trainer: pl.Trainer,
         lr: float = 1.0e-3,
         use_wandb: bool = False,
         seed: Optional[int] = 42,
     ):
         super().__init__()
-        self.save_hyperparameters()
+        self.save_hyperparameters("lr", "use_wandb", "seed")
         self.datamodule = datamodule
         self.encoder = encoder
         self.clusterer = clusterer
-
-        self.trainer = pl.Trainer(max_epochs=epochs)
-        self.pt_trainer = pl.Trainer(max_epochs=pretrain_epochs)
+        self.trainer = trainer
+        self.pt_trainer = pt_trainer
 
     def configure_optimizers(self) -> Optimizer:
-        return Adam(self.parameters(), lr=self.hparams.lr)
+        return Adam(self.encoder.parameters(), lr=self.hparams.lr)
 
     def training_step(self, batch: Tensor, batch_idx: int) -> Tensor:
         x = batch[0] if isinstance(batch, Sequence) else batch
         encoding = self.encoder(x)
         _ = self.clusterer.fit_transform(encoding)
-        return encoding.sum()
+        loss = encoding.sum()
+        self.log("train_loss", loss)
+        tqdm_dict = {"loss": loss}
+        output = OrderedDict({"loss": loss, "progress_bar": tqdm_dict, "log": tqdm_dict})
+        return output
 
-    def start(self, raw_config: Optional[Dict[str, Any]]):
+    def start(self, raw_config: Optional[Dict[str, Any]] = None):
         self.datamodule.setup()
         if self.hparams.use_wandb:
             wandb.init(entity="predictive-analytics-lab", project="topocluster", config=raw_config)
+            self.pt_trainer.logger = WandbLogger()
+            self.trainer.logger = WandbLogger()
         pl.seed_everything(seed=self.hparams.seed)
         self.encoder.build(self.datamodule.dims)
         self.pt_trainer.fit(self.encoder, datamodule=self.datamodule)
-        self.trainer.fit(self, datamodule=self.datamodule)
+        self.trainer.fit(self.encoder, datamodule=self.datamodule)
