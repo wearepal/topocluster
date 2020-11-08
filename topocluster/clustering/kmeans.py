8a6af1315914410892ca2a340b80236476eb1171
Myles Bartlett <m.bartlett@sussex.ac.uk>
2020-11-08 23:38:26 +0000

    .

--- topocluster/clustering/kmeans.py
+++ topocluster/clustering/kmeans.py
@@ -37,7 +37,7 @@ class Kmeans(Clusterer):
         self,
         n_iter: int,
         k: Optional[int] = None,
-        backend: Backends = Backends.FAISS,
+        backend: Backends = Backends.TORCH,
         verbose: bool = False,
     ):
         self.k = k
