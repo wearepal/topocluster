3b0bbfbb81c9bd0f4c4a714a02355b056a8cba41
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-04-30 15:40:52 +0100

    Condense _get_dists.

--- topocluster/data/sampling.py
+++ topocluster/data/sampling.py
@@ -67,33 +67,42 @@ class GreedyCoreSetSampler(Sampler[List[int]]):
         num_images = batch.size(0)
         dist_mat = batch @ batch.t()
         sq = dist_mat.diagonal().view(num_images, 1)
-        dist_mat *= -2
-        dist_mat += sq
-        dist_mat += sq.t()
-        return dist_mat
+        return -2 * dist_mat + sq + sq.t()
 
     @implements(Sampler)
     def __iter__(self) -> Iterator[List[int]]:
+        # iterative forever (until some externally defined stopping-criterion is reached)
         while 1:
-            # Frist sample the 'oversampled' batch from which to construc the core-set
+            # Frist sample the 'oversampled' batch from which to construct the core-set
             os_batch_idxs = torch.randperm(self.budget)[: self._num_oversampled_samples]
             # Compute the euclidean distance between all pairs in said batch
             dists = self._get_dists(os_batch_idxs)
-            # greedy k-center core-set construction algorithm
+            # Mask indicating whether a sample is still yet to be sampled (1=unsampled, 0=sampled)
+            # - updating a mask is far more efficnet than reconstructing the list of unsampled
+            # indexes every iteration (however, we do have to be careful about the 'meta-indexing'
+            # it introduces)
             unsampled_m = torch.ones_like(os_batch_idxs, dtype=torch.bool)
+            # there's no obvious decision rule for seeding the core-set so we just select the first
+            # point arbitrarily, moving it from the unsampled pool to the sampled one by setting
+            # its corresponding mask-value to 0
             sampled_idxs = [int(os_batch_idxs[0])]
             unsampled_m[0] = 0
 
+            # Begin the furtherst-frist lraversal algorithm
             while len(sampled_idxs) < self.budget:
                 unsampled_idxs = os_batch_idxs[unsampled_m]
-                # p := argmax min_{i\inB}(d(x, x_i)); i.e. select the sample which maximizes the
-                # minimum distance (euclidean norm) to all previously selected samples
+                # p := argmax min_{i\inB}(d(x, x_i)); i.e. select the point which maximizes the
+                # minimum squared Euclidean-distance to all previously selected points
+                # NOTE: The argmax index is relative to the unsampled indexes
                 rel_idx = torch.argmax(
                     torch.min(dists[~unsampled_m][:, unsampled_idxs], dim=0).values
                 )
-                p = unsampled_idxs[rel_idx]
+                # Retrieve the index corresponding to the previously-computed argmax index
+                to_sample = unsampled_idxs[rel_idx]
+                sampled_idxs.append(int(to_sample))
+                # Update the mask, which corresponds to moving the sampled index from the unsampled
+                # pool to the sampled pool
                 unsampled_m[unsampled_m.nonzero()[rel_idx]] = 0
-                sampled_idxs.append(int(p))
 
             yield sampled_idxs
             del dists, unsampled_m, sampled_idxs
